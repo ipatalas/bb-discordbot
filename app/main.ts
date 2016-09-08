a607bfbf477795c6c9c168ef50ac8f70b7db3202
Ireneusz Patalas <fido@list.pl>
2016-09-08 21:27:24 +0200

    Initial commit

---
+++ app/main.ts
@@ -0,0 +1,11 @@
+/// <reference path="../typings/index.d.ts" />
+
+import { BarType, ClassA } from './package';
+import * as express from "express";
+
+var t : BarType;
+var test : ClassA = new ClassA('test');
+
+var app = express(); 
+
+console.log(`Test message - ${test.name}`);
---
+++ app/package.ts
@@ -0,0 +1,10 @@
+
+export interface BarType {
+    bar: number;
+}
+
+export class ClassA {
+    constructor(public name: string) {
+        
+    }
+}
