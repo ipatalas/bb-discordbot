0264fc8c24dc2f742bf1e01ab65bfeb90e711607
Ireneusz Patalas <fido@list.pl>
2016-09-12 20:47:45 +0200

    Updated gulpfile to improve development cycle

--- gulpfile.js
+++ gulpfile.js
@@ -4,33 +4,38 @@ var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var tslint = require('gulp-tslint');
 var clean = require('gulp-clean');
+var nodemon = require('gulp-nodemon');
 var runSequence = require('run-sequence');
 var config = require('./gulp.config.js')();
- 
-var tsProject = ts.createProject('tsconfig.json');
- 
-gulp.task('ts', function() {
-    var tsResult = tsProject.src(config.ts.allTs)
-        .pipe(ts(tsProject));
 
-    return tsResult.js.pipe(gulp.dest(config.build.output));
+var tsProject = ts.createProject('tsconfig.json');
+
+gulp.task('ts', function () {
+	var tsResult = tsProject.src(config.ts.allTs)
+		.pipe(ts(tsProject));
+
+	return tsResult.js.pipe(gulp.dest(config.build.output));
 });
- 
+
 gulp.task('ts-lint', function () {
-    return gulp.src(config.ts.allTs)
-        .pipe(tslint({formatter: "prose"}))
-        .pipe(tslint.report());
+	return gulp.src(config.ts.allTs)
+		.pipe(tslint({ formatter: "prose" }))
+		.pipe(tslint.report());
 });
 
 gulp.task('clean', function (cb) {
-    return gulp.src(config.js.all, {read: false})
-        .pipe(clean());
+	return gulp.src(config.js.all, { read: false })
+		.pipe(clean());
 });
 
 gulp.task('watch', function() {
-    gulp.watch([config.ts.allTs], ['ts-lint', 'ts']);
+	gulp.watch(config.ts.allTs, ['ts']);
 });
 
-gulp.task('default', function(cb) {
-    runSequence('ts-lint', 'ts', cb);
+gulp.task('develop', ['ts', 'watch'], function() {
+	nodemon({script: config.build.main});
+});
+
+gulp.task('default', function (cb) {
+	runSequence('ts-lint', 'ts', cb);
 });
