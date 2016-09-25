fecc18355d0d8e0b843d612f3bf2cca21914ec04
Ireneusz Patalas <fido@list.pl>
2016-09-25 21:33:09 +0200

    Reworked operations dictionary

--- app/operations.ts
+++ app/operations.ts
@@ -3,59 +3,83 @@
 import * as _ from "lodash";
 
 export enum OperationType {
-	MilkRun = <any>"Milk Run",
-	EarlyBird = <any>"Early Bird",
-	VenusFlytrap = <any>"Venus Flytrap",
-	FullMonty = <any>"Full Monty",
-	Pencilneck = <any>"Pencilneck",
-	Charleston = <any>"Charleston",
-	TickerTape = <any>"Ticker Tape",
-	TankTango = <any>"Tank Tango",
-	Hacksaw = <any>"Hacksaw",
-	UpperLip = <any>"Upper Lip",
-	PowderKeg = <any>"Powder Keg",
-	Mambo = <any>"Mambo",
-	SourGrapes = <any>"Sour Grapes",
-	Bottleneck = <any>"Bottleneck",
-	Tinderbox = <any>"Tinderbox",
-	Foxtrot = <any>"Foxtrot",
-	Stronghold = <any>"Stronghold",
-	ChokePoint = <any>"Choke Point",
-	CurtainCall = <any>"Curtain Call",
-	DeadEnd = <any>"Dead End",
-	DeepCut = <any>"Deep Cut",
-	MassiveAttack = <any>"Massive Attack"
+	MilkRun = 1,
+	EarlyBird,
+	VenusFlytrap,
+	FullMonty,
+	Pencilneck,
+	Charleston,
+	TickerTape,
+	TankTango,
+	Hacksaw,
+	UpperLip,
+	PowderKeg,
+	Mambo,
+	SourGrapes,
+	Bottleneck,
+	Tinderbox,
+	Foxtrot,
+	Stronghold,
+	ChokePoint,
+	CurtainCall,
+	DeadEnd,
+	DeepCut,
+	MassiveAttack
+}
+
+class Operation {
+	constructor(public displayName: string, public aliases: string[]) {}
+
+	public matches(aliasOrName: string): boolean {
+		let lowerCaseName = aliasOrName.toLowerCase();
+
+		return (this.displayName.toLowerCase() === lowerCaseName)
+			|| (_.includes(this.aliases, lowerCaseName));
+	}
 }
 
 export class Operations {
-	private static aliases: { [key: number]: string[]; } = {
-		[OperationType.MilkRun]: ["milk"],
-		[OperationType.EarlyBird]: ["bird"],
-		[OperationType.VenusFlytrap]: ["venus"],
-		[OperationType.FullMonty]: ["monty"],
-		[OperationType.Pencilneck]: ["pencil"],
-		[OperationType.Charleston]: ["charles"],
-		[OperationType.TickerTape]: ["ticker"],
-		[OperationType.TankTango]: ["tango"],
-		[OperationType.Hacksaw]: ["hack"],
-		[OperationType.UpperLip]: ["upper"],
-		[OperationType.PowderKeg]: ["powder"],
-		[OperationType.Mambo]: ["mambo"],
-		[OperationType.SourGrapes]: ["sour"],
-		[OperationType.Bottleneck]: ["bottle"],
-		[OperationType.Tinderbox]: ["tinder"],
-		[OperationType.Foxtrot]: ["fox"],
-		[OperationType.Stronghold]: ["strong", "sg"],
-		[OperationType.ChokePoint]: ["choke", "cp"],
-		[OperationType.CurtainCall]: ["cc"],
-		[OperationType.DeadEnd]: ["de"],
-		[OperationType.DeepCut]: ["dc"],
-		[OperationType.MassiveAttack]: ["ma"],
+	private static operations: { [key: number]: Operation; } = {
+		[OperationType.MilkRun]: new Operation("Milk Run", ["milk"]),
+		[OperationType.EarlyBird]: new Operation("Early Bird", ["bird"]),
+		[OperationType.VenusFlytrap]: new Operation("Venus Flytrap", ["venus"]),
+		[OperationType.FullMonty]: new Operation("Full Monty", ["monty"]),
+		[OperationType.Pencilneck]: new Operation("Pencilneck", ["pencil"]),
+		[OperationType.Charleston]: new Operation("Charleston", ["charles"]),
+		[OperationType.TickerTape]: new Operation("Ticker Tape", ["ticker"]),
+		[OperationType.TankTango]: new Operation("Tank Tango", ["tango"]),
+		[OperationType.Hacksaw]: new Operation("Hacksaw", ["hack"]),
+		[OperationType.UpperLip]: new Operation("Upper Lip", ["upper"]),
+		[OperationType.PowderKeg]: new Operation("Powder Keg", ["powder"]),
+		[OperationType.Mambo]: new Operation("Mambo", ["mambo"]),
+		[OperationType.SourGrapes]: new Operation("Sour Grapes", ["sour"]),
+		[OperationType.Bottleneck]: new Operation("Bottleneck", ["bottle"]),
+		[OperationType.Tinderbox]: new Operation("Tinderbox", ["tinder"]),
+		[OperationType.Foxtrot]: new Operation("Foxtrot", ["fox"]),
+		[OperationType.Stronghold]: new Operation("Stronghold", ["strong", "sh"]),
+		[OperationType.ChokePoint]: new Operation("Choke Point", ["choke", "cp"]),
+		[OperationType.CurtainCall]: new Operation("Curtain Call", ["cc"]),
+		[OperationType.DeadEnd]: new Operation("Dead End", ["de"]),
+		[OperationType.DeepCut]: new Operation("Deep Cut", ["dc"]),
+		[OperationType.MassiveAttack]: new Operation("Massive Attack", ["ma"])
+	}
+
+	static isEnumIndex(key): boolean {
+		const n = ~~Number(key);
+		return String(n) === key && n >= 0;
 	}
 
 	static getOperation(aliasOrName: string) {
-		for (var key in this.aliases) {
-			if (_.includes(this.aliases[key], aliasOrName)) {
+		for (var key in OperationType) {
+			if (!this.isEnumIndex(key)) {
+				continue;
+			}
+
+			if (aliasOrName.toLowerCase() === OperationType[key].toLowerCase()) {
+				return key;
+			}
+
+			if (this.operations[key].matches(aliasOrName)) {
 				return key;
 			}
 		}
